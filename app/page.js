794aef161e73de9460e834ce4397b57985eb6872
Shivam <145973348+shivamyeshu@users.noreply.github.com>
2024-11-01 00:09:21 +0530

    2nd test done

--- app/page.js
+++ app/page.js
@@ -1,23 +1,61 @@
-// import Image from "next/image";
+// // import Image from "next/image";
+// import React from "react";
+// import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+
+// export default function Home() {
+//   return (
+   
+//       <BackgroundBeamsWithCollision>
+//         <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
+//           this is the trial of aceternity{" "}
+//           <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+//             <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+//               <span className="">by shivam.</span>
+//             </div>
+//             <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+//               <span className="">by shivam.</span>
+//             </div>
+//           </div>
+//         </h2>
+//       </BackgroundBeamsWithCollision>
+//     );
+
+// }
+
+// ______________________________________________________________________________________//
+
+
+"use client";
 import React from "react";
-import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import Image from "next/image";
 
 export default function Home() {
   return (
-   
-      <BackgroundBeamsWithCollision>
-        <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
-          this is the trial of aceternity{" "}
-          <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-            <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-              <span className="">by shivam.</span>
-            </div>
-            <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-              <span className="">by shivam.</span>
-            </div>
-          </div>
-        </h2>
-      </BackgroundBeamsWithCollision>
-    );
-
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold text-black dark:text-white">
+              Unleash the power of <br />
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                Scroll Animations
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <Image
+          src={`/linear.webp`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
+  );
 }
+
+
